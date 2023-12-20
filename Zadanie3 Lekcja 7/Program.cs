0ec04179df96ef914916fc837efef88df76c0d65
HARDPC <HARDPC@WOTUM>
2023-12-20 18:34:05 +0100

    Add project files.

---
+++ Zadanie3 Lekcja 7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KatoApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Nadanie zmiennej int wartości
+            int liczba = 14;
+
+
+            //Porównanie zmiennych przy użyciu operatora 
+            if (liczba > 0)
+            {
+                Console.WriteLine(liczba + " jest liczbą dodatnią"); //Wyświetlenie komunikatu, gdy liczba jest dodatnia
+            }
+            else if (liczba < 0)
+            {
+                Console.WriteLine(liczba + " jest liczbą ujemną"); //Wyświetlenie komunikatu, gdy liczba jest ujemna
+            }
+                             
+        }
+
+    }
+
+
+}
